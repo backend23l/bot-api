39bcb198d5d8d5c3a436ac53a4d694c79b1c4f97
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-06 15:47:35 +0500

    reply keyboard markup

--- send_message.py
+++ send_message.py
@@ -2,14 +2,34 @@ import requests
 from settings import URL, chat_id
 
 
-def send_message(url: str, chat_id: int, text: str):
+def send_message(url: str, chat_id: int, text: str, reply_markup: dict):
     endpoint = '/sendMessage'
     url += endpoint
 
-    payload = {
+    data = {
         'chat_id': chat_id,
         'text': text,
+        'reply_markup': reply_markup,
     }
-    requests.get(url, params=payload)
+    requests.post(url, json=data)
 
-send_message(URL, chat_id, 'hi')
+btn1 = {
+    "text": "share contact",
+    "request_contact": True
+}
+btn2 = {
+    "text": "location",
+    "request_location": True
+}
+btn3 = {"text": "button 3"}
+
+reply_markup = {
+    "keyboard": [
+        [btn1, btn2],
+        [btn3]
+    ],
+    "resize_keyboard": True,
+    # "one_time_keyboard": True,
+}
+
+send_message(URL, chat_id, 'hi', reply_markup)
